32ef5b11ab3f05054f7ef296d2833922f4cb8b37
alfred1112 <45417453+alfred1112@users.noreply.github.com>
2022-06-17 15:07:53 +0800

    getAverage

--- src/main/java/com/brian/score/Student.kt
+++ src/main/java/com/brian/score/Student.kt
@@ -1,11 +1,23 @@
 package com.brian.com.brian.score
 
-class Student {
+class Student (var name:String,var english:Int,var math:Int){
 //    var name = ""
 //    var english = 0
 //    var math = 0
+   init {
+//        println("Testing")
+    }
+    fun print(){
+//        println(name + "\t" + english +"\t" +math)
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage() :Int{
+        return(english+math)/2
+    }
+
 }
 
 fun main(){
     val stu  = Student("Jack", 55, 87)
+    stu.print()
 }
